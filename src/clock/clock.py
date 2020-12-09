fe2adc0031c22ab3108d456e05dd8e9c32be9ec2
Admin <huutam.ptnk@gmail.com>
2020-12-09 04:00:00 +0200

    Update clock component

--- src/clock/clock.py
+++ src/clock/clock.py
@@ -1,16 +1,18 @@
 import time 
 
 class Clock: 
-    def __init__(self, name): 
+    def __init__(self, name, ): 
         self.name = name
     
     def run(self):
         while (True):
+            # Get current time in local machine
+            current_time = time.strftime("%a, %d %b %Y %H:%M:%S", time.localtime())
+            print(f'{self.name}: {current_time}')  
+            
             time.sleep(1)
-            currentTime = time.time()
-            print(f'{self.name}: {currentTime}')  
 
 if __name__ == "__main__":
-    newClock = Clock("Clock 1")
-    newClock.run()
+    new_clock = Clock("Clock 1")
+    new_clock.run()
     
