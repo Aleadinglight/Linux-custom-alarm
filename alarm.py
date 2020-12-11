dee6e14f678e0a92ae82d4f242f68170ea858d1c
Admin <huutam.ptnk@gmail.com>
2020-12-11 10:23:00 +0200

    Update alarm main component

--- alarm.py
+++ alarm.py
@@ -4,3 +4,4 @@ import argparse
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="This program is a custom alarm clock where you can choose the clock sound and personal message.")
+    parser.add_argument("run", dest="command", action="store_const", const="delete", help="Start the clock")
