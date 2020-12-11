4bae5c0a655f9e650280245bb1130654e403aaef
Admin <huutam.ptnk@gmail.com>
2020-12-11 10:20:00 +0200

    Update clock component

---
+++ alarm.py
@@ -0,0 +1,6 @@
+import argparse
+
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="This program is a custom alarm clock where you can choose the clock sound and personal message.")
