1674922dcfc6ddaadaaee516c8922d76d3c45b8c
Admin <huutam.ptnk@gmail.com>
2020-12-14 14:00:00 +0200

    Update alarm.py

--- alarm.py
+++ alarm.py
@@ -4,4 +4,13 @@ import argparse
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="This program is a custom alarm clock where you can choose the clock sound and personal message.")
-    parser.add_argument("run", dest="command", action="store_const", const="delete", help="Start the clock")
+    
+    # -t, --timer-list 
+    
+
+    # Get the arguments from the user
+    args = parser.parse_args()
+    
+    # Processing the arguments
+    if args.command == True:
+        print("Run clock")
