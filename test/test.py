4de0f90da28dc3dc1db4edd7c53a4a5493795d2a
Admin <huutam.ptnk@gmail.com>
2020-12-30 17:05:00 +0200

    update test.py

--- test/test.py
+++ test/test.py
@@ -1,4 +1,11 @@
+import sys
+from pathlib import Path
 
+# Import local module
+currentFilePath = Path(__file__).resolve().parents[1]
+
+sys.path.append(str(currentFilePath.joinpath("app","timerio")))
+from timerio import TimerIO
 
 def testTimerIO():
     print("test timer io")
