645ce85e39a2d5b1f45f6c0d59b0fc95c3041a74
Admin <huutam.ptnk@gmail.com>
2020-12-30 15:00:00 +0200

    update clock timer

--- app/timerio/timerio.py
+++ app/timerio/timerio.py
@@ -1,10 +1,29 @@
 from pathlib import Path
 
-print(Path(__file__).resolve().parents[2].joinpath("resources","timers"))
-
 class TimerIO:
     def __init__(self, timer_directory=None):
         if (timer_directory != None):
             self.timer_directory = timer_directory
         else:
             self.timer_directory = Path(__file__).resolve().parents[2].joinpath("resources","timers")
+        
+        self.timer_file_name = "timers.json"
+            
+    def getTimerList(self):
+        print(1)
+    
+    def saveToFile(self, entry):
+        print("Save to")
+    
+    def getTimerDirectory(self):
+        return self.timer_directory
+    
+    def getTimerFileName(self):
+        return self.timer_file_name
+    
+    def append(self, entry):
+        print(1)
+    
+    def delete(self, id):
+        print(2)
+        
