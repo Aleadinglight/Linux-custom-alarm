cc7f40b51e5ca0a6c6131daebd354ae7d94a419e
Admin <huutam.ptnk@gmail.com>
2021-01-07 09:34:21 +0200

    Update alarm CLLI

--- alarm.py
+++ alarm.py
@@ -7,13 +7,15 @@ if __name__ == "__main__":
         you can choose the clock sound and personal message.")
 
     parser.add_argument("-a", 
-                        "--add-new-timer",
-                        help="Add a new timer to the clock",
-                        dest="timer")
+                        "--add-timer",
+                        help="add a new timer to the clock",
+                        action="store",
+                        metavar=("<timer>","<alarm-sound>"),
+                        dest="timer",
+                        nargs=2)
     
     # Get the arguments from the user
     args = parser.parse_args()
     
     # Processing the arguments
-    if args.command == True:
-        print("Run clock")
+    print(args)
